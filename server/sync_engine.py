0eb2817f34a39eb7de763d90ba8fa20d7a3de795
Aneek Roy <aneeknroy@gmail.com>
2025-04-29 12:20:43 -0500

    AR

--- server/sync_engine.py
+++
@@ -1,6 +0,0 @@
-def apply_edit(current_text, edit):
-    from_index = edit['from']
-    to_index = edit['to']
-    new_text = edit['text']
-    # Apply simple text replacement logic
-    return current_text[:from_index] + new_text + current_text[to_index:]
